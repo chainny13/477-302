7d613de0d1d7c22ab4647b6149e91fc1814224e4
chainny13 <chainny56>
2021-07-05 10:30:15 +0700

    how to ccs

---
+++ Untitled-1.vb
@@ -0,0 +1,10 @@
+
+CSS Component
+1.selector > elment, id, class name
+2.declaration
+2.1 property > color , text-style, border, text-size, ....
+2.2 value > red, #ffaabb, bold, italic, 2px, 2em, 24px
+
+1 selector > 1 declaration
+
+div { color:red; border:2px }
